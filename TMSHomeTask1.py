91cc6d3604d9a2d9128909c16e9c1282967cbe7d
Evgeniy Polyakov <vip.zhenya.polyakov.00@mail.ru>
2022-08-25 13:26:12 +0300

    Add file

---
+++ TMSHomeTask1.py
@@ -0,0 +1,56 @@
+# Exz 1
+def numbers_sum():
+    number1 = int(input('Введите первое число: '))
+    number2 = int(input('Введите второе число: '))
+    return number1 + number2
+
+
+def differences_sum():
+    number1 = int(input('Введите первое число: '))
+    number2 = int(input('Введите второе число: '))
+    return number1 / number2
+
+
+def product_of_numbers():
+    number1 = int(input('Введите первое число: '))
+    number2 = int(input('Введите второе число: '))
+    return number1 * number2
+
+
+def degree():
+    number1 = int(input('Введите первое число: '))
+    number2 = int(input('Введите второе число: '))
+    return number1 ** number2
+
+
+def mod():
+    number1 = int(input('Введите первое число: '))
+    number2 = int(input('Введите второе число: '))
+    return number1 % number2
+
+
+def integer_division():
+    number1 = int(input('Введите первое число: '))
+    number2 = int(input('Введите второе число: '))
+    return number1 // number2
+
+
+print(integer_division())
+
+# Exz 2
+x = (17 / 2 * 3) + 2
+y = 2 + (17 / 2 * 3)
+z = (19 % 4) + (15 / 2 * 3)
+w = (15 + 6) - (10 * 4)
+t = ((17 / 2 % 2) * 3 ** 3)
+
+# Exz 3
+
+first_abs = int(input("Введите первый модуль: "))
+second_abs = int(input("Введите второй модуль: "))
+
+print(abs(first_abs) - abs(second_abs) / 1 + abs(first_abs + second_abs))
+
+# Exz 4
+
+# True; True; False; False; True; True; False; True; True; True; True; 24; 162; 2.5
