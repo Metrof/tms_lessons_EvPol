e20eb34a81361f50cf3e9a5e9ef807ee618ff1b5
Evgeniy Polyakov <vip.zhenya.polyakov.00@mail.ru>
2022-08-27 14:38:11 +0300

    Add file

---
+++ TMSHomeTask2.py
@@ -0,0 +1,33 @@
+# Ex 1
+
+multiples_of_7 = [7, 14, 21, 28, 35, 42, 49, 56, 63, 70, 77, 84, 91, 98]
+first_five = multiples_of_7[:5]
+assert [7, 14, 21, 28, 35] == first_five
+
+# Ex 2
+
+from collections import Counter
+
+NUMBERS = [4, 1, 8, 4, 0, 2, 8, 1, 0, 4]
+
+count = Counter(NUMBERS)  # Получает словарь с ключем - значением элемента, значением - количество идентичных элементов в списке
+item_list = list(count.items())
+for i in range(len(count)):
+    current_value = count.popitem()
+    if current_value[1] > 1:
+        for b in range(current_value[1] - 1):  # Если в списке несколько идентичных элементов, то цикл удаляет первые по списку дубликаты оставляя один
+            NUMBERS.remove(current_value[0])
+assert [2, 8, 1, 0, 4] == NUMBERS
+
+# Ex 3
+
+set_list = set(NUMBERS)
+new_list = list(set_list)
+assert [0, 1, 2, 4, 8] == new_list
+
+# Ex 4
+
+CITIES = ('Minsk', 'Gomel', 'Mogivel', 'Orsha', 'Vitebsk', 'Grodno')
+cities_list = list(CITIES)
+cities_list.append('Brest')
+assert ['Minsk', 'Gomel', 'Mogivel', 'Orsha', 'Vitebsk', 'Grodno', 'Brest'] == cities_list
